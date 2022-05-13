79f55b49044a9cc938f4a8204adfe748b020d968
Cameron <cdg341@gmail.com>
2022-05-13 13:41:08 -0500

    Add general layout structure

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector("button");
+
+button.addEventListener("click", () => (popupForm.style.display = "block"));
+
+let myLibrary = ["Harry Potter", "The Harder Boys", "Star Wars"];
+
+function Book() {}
+
+function addBookToLibrary() {}
