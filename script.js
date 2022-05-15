13bad16ee924041106818e62131639bb3d6abd39
Cameron <cdg341@gmail.com>
2022-05-15 01:19:23 -0500

    Take users input and add it to the table

--- script.js
+++ script.js
@@ -2,8 +2,37 @@ let button = document.querySelector("button");
 let title = document.getElementById("title");
 let author = document.getElementById("author");
 let pages = document.getElementById("pages");
+const table = document.querySelector("table");
+const checkbox = document.getElementById("checkbox");
 
-button.addEventListener("click", () => (popupForm.style.display = "block"));
+button.addEventListener("click", () => {
+  const titleData = document.createElement("td");
+  const authorData = document.createElement("td");
+  const pagesData = document.createElement("td");
+  const readData = document.createElement("td");
+  const tr = document.createElement("tr");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.classList.add("deleteBtn");
+
+  titleData.textContent = title.value;
+  authorData.textContent = author.value;
+  pagesData.textContent = pages.value;
+  deleteBtn.textContent = "Delete";
+
+  if (checkbox.checked) {
+    readData.textContent = "I have read";
+  } else {
+    readData.textContent = "I have not read";
+  }
+
+  table.appendChild(titleData);
+  table.appendChild(authorData);
+  table.appendChild(pagesData);
+  table.appendChild(readData);
+  //   table.appendChild(deleteBtn);
+  table.appendChild(tr);
+});
 
 let myLibrary = ["Harry Potter", "The Harder Boys", "Star Wars"];
 
